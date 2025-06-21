39aaad76ca3d01c1f084f03e7ca01a4549346f24
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-06-22 01:10:20 +0600

    object completed

---
+++ Week 1 - HTML, CSS and Basic Js/Basics of Javascript/index.js
@@ -0,0 +1,70 @@
+//  Variables
+
+var a = 10;
+console.log("Before");
+console.log(a);
+a = 20;
+console.log("After");
+console.log(a);
+
+// Array
+const arr = [1, 2, "Adnan"];
+console.log(arr[0]);
+
+// Functions
+function hello(name) {
+  return "Hello " + name;
+}
+
+const greet = hello("Adnan");
+console.log(greet);
+
+// conditions
+
+age = 18;
+if (age >= 18) {
+  console.log("Can Vote");
+} else {
+  console.log("Can't Vote");
+}
+
+// make it in function
+
+function canVote(age) {
+  if (age >= 18) {
+    return "Can Vote";
+  } else {
+    return "Can't Vote";
+  }
+}
+
+let ans = canVote(10);
+console.log(ans);
+
+// Array
+
+const names = [
+  "Alice",
+  "Bob",
+  "Charlie",
+  "David",
+  "Eva",
+  "Frank",
+  "Grace",
+  "Helen",
+];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+// object
+
+let user = {
+  name: "Adnan",
+  age: 10,
+};
+
+console.log(user.name);
+console.log(user["age"]);
+
