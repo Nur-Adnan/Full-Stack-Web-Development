6e3f0c0ea0cba41f94c8713cc0e9c4896b583777
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2025-06-22 11:43:44 +0600

    find the sum from 1 to a number, completed

---
+++ Week 2 - Async JS/index.js
@@ -0,0 +1,12 @@
+// find the sum from 1 to a number
+
+function sum(n) {
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+
+const ans = sum(10);
+console.log(ans);
